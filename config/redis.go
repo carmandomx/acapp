c896cb8b3ff7cf5400af7447c060c81bccd43839
Carlos Reyes <carmandoreyes93@gmail.com>
2021-06-03 17:47:29 -0500

    added env var for redis

--- config/redis.go
+++ config/redis.go
@@ -2,13 +2,17 @@ package config
 
 import (
 	// "github.com/go-redis/redis/v7"
+
+	"os"
+
 	"github.com/go-redis/redis/v8"
 )
 
 var Redis *redis.Client
 
 func CreateRedisClient() {
-	opt, err := redis.ParseURL("redis://localhost:6379/0")
+
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
 
 	if err != nil {
 		panic(err)
